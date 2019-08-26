3f2329f0b8f3aac16f52b3f36c923bc05f692917
bugcloud <bugcloudisco@bugcloud.com>
2019-08-27 03:05:25 +0900

    Add target_cpa_local_micro to TwitterAds::LineItem

--- lib/twitter-ads/campaign/line_item.rb
+++ lib/twitter-ads/campaign/line_item.rb
@@ -34,6 +34,7 @@ module TwitterAds
     property :primary_web_event_tag
     property :product_type
     property :start_time, type: :time
+    property :target_cpa_local_micro
     property :total_budget_amount_local_micro
 
     # beta (not yet generally available)
